1e7145e9b777f5721006b04a4d929216acff0ab8
orpartz <orptzz@gmail.com>
2020-02-12 12:16:34 +0200

    phase1 commit

--- controllers/meme-controller.js
+++ controllers/meme-controller.js
@@ -1,3 +1,45 @@
 'use strict';
 
-console.log('controller')
+var gCanvas;
+var gCtx;
+
+function onInit() {
+  gCanvas = document.querySelector('canvas');
+  gCtx = gCanvas.getContext('2d');
+  drawImgWithTxt();
+}
+
+function renderCanvas() {
+  drawImgWithTxt();
+}
+
+function drawImgWithTxt() {
+  let img = new Image();
+  img.src = getImgUrl(gMeme.selectedImgId);
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+    let memeTxt = getMemeTxt();
+    drawText(memeTxt, 250, 50);
+  }
+}
+
+function drawText(text, x, y) {
+  gCtx.lineWidth = '2'
+  gCtx.strokeStyle = 'black'
+  gCtx.fillStyle = 'white'
+  gCtx.font = '50px Impact'
+  gCtx.textAlign = 'center'
+  gCtx.fillText(text, x, y)
+  gCtx.strokeText(text, x, y)
+}
+
+function onTxtChange(elTxt) {
+  let userTxt = elTxt.value;
+  updateMemeTxt(userTxt);
+  renderCanvas();
+}
+
+function onChooseImg(elImg) {
+  gMeme.selectedImgId = +elImg.dataset.id;
+  renderCanvas()
+}
--- services/meme-service.js
+++ services/meme-service.js
@@ -1,2 +1,46 @@
 'use strict';
-console.log('service')
+
+var gKeywords = {
+  'happy': 12,
+  'funny puk': 1
+}
+
+var gImgs = [
+  {
+    id: 1,
+    url: 'img/1.jpg',
+    keywords: ['happy']
+  },
+  {
+    id: 2,
+    url: 'img/2.jpg',
+    keywords: ['animals']
+  },
+];
+
+var gMeme = {
+  selectedImgId: 1,
+  selectedLineIdx: 0,
+  lines: [{
+    //from the user, this is the default
+    txt: 'I never eat Falafel', 
+    size: 20,
+    align: 'left',
+    color: 'red'
+  }]
+}
+
+function getImgUrl(imgId) {
+  let desiredImg = gImgs.find(img => img.id === imgId);
+  return desiredImg.url;
+}
+
+function getMemeTxt() {
+  return gMeme.lines[0].txt;
+}
+
+function updateMemeTxt(txt) {
+  let lineIdx = gMeme.lines.length - 1;
+  gMeme.lines[lineIdx].txt = txt;
+  console.log(gMeme.lines)
+}
