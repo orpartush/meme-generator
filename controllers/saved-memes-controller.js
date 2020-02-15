9d1017b8e0720f525bb92c42f4730a3e2b7e5239
orpartz <orptzz@gmail.com>
2020-02-15 20:50:20 +0200

    save memes update2

--- controllers/saved-memes-controller.js
+++ controllers/saved-memes-controller.js
@@ -7,6 +7,10 @@ function onInit() {
 
 function renderMemes() {
   let elContainer = document.querySelector('.memes-container');
+  if (gSavedMemes.length === 0) {
+      elContainer.innerHTML = `<h1>You didn't saved any memes yet</h1>`;
+      return;
+  }
   gSavedMemes.forEach(meme => {
     let img = new Image();
     img.src = `${meme}`;
